35a336f91a38eb42f9d3fe552ca9e25540d2b730
mathieu enault <me94110@gmail.com>
2023-11-10 15:10:27 +0100

    fix db link issue, bad imports and some style improvement

--- database/index.js
+++ database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://enaultmathieu2:qwe@cluster0.wt48ajo.mongodb.net/?retryWrites=true&w=majority"
+    "mongodb+srv://enaultmathieu2:me94110@cluster0.wt48ajo.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => console.log("connexion db ok !"))
   .catch((err) => console.log(err));
